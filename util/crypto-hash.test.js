e425d3c5d91d9a45ee707e9d8a3d9115ce150a28
apoorvamohite <apps.mohite@gmail.com>
2019-11-03 22:33:48 +0530

    Updated transaction.update()

--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -3,10 +3,17 @@ const cryptoHash = require('./crypto-hash');
 describe('cryptoHash()', () => {
 
     it('generates a SHA-256 hashed output', () => {
-        expect(cryptoHash('Hello Blockchain')).toEqual("7cf88f2ee398c0b7c0e760a1dccaf3571e0baccf310f11fe3bdfd0b09675ea75");
+        expect(cryptoHash('Hello Blockchain')).toEqual("9867e35e8af7e4446f5cbd032eedd5e05771e3f8f85ceafd579b4a8a1c5bc1f7");
     });
 
     it('produces the same hash with the same input arguments in any order', () => {
         expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('three', 'one', 'two'));
+    });
+
+    it('produces a different hash when the properties of the input change', () => {
+        const foo = {};
+        originalHash = cryptoHash(foo);
+        foo['a'] = 'a';
+        expect(cryptoHash(foo)).not.toEqual(originalHash);
     });
 });
