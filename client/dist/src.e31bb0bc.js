59dd657185dd12bf6cacd721f947fae7dc95b4ff
Apoorva Mohite <apps.mohite@gmail.com>
2021-04-13 21:45:50 +0530

    Fixed rmdir issue

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -47587,7 +47587,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51186" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51481" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
